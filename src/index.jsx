fb5e7163608282ee71d5bb85889dfebecfeed0c5
AnroidWays <bubakzampu@gmail.com>
2025-04-14 05:11:01 +0300

    formatting

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom'
-import App from './App'
-import './styles.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('app')).render(
-  <Router>
-    <App />
-  </Router>,
-)
+    <BrowserRouter
+        future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+    >
+        <App />
+    </BrowserRouter>
+);
